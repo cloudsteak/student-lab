66e68345012f5ddddb1e2eb76b003c9bc12a6420
the1bit <tibor.kiss@cloudmentor.hu>
2025-03-04 00:19:50 +0100

    student_password

--- aws/labs/basic/outputs.tf
+++ aws/labs/basic/outputs.tf
@@ -2,11 +2,6 @@ output "student_username" {
   value = module.student.student_username
 }
 
-output "student_password" {
-  value     = module.student.student_password
-  sensitive = true
-}
-
 
 output "aws_console_url" {
   value = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console"
--- aws/modules/student/outputs.tf
+++ aws/modules/student/outputs.tf
@@ -2,8 +2,3 @@
 output "student_username" {
   value = aws_iam_user.student.name
 }
-
-output "student_password" {
-  value     = aws_iam_user_login_profile.student_password.password
-  sensitive = true
-}
