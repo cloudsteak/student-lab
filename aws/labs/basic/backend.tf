2a94bf85548ebc5f1d5f9a0b6cbc95950f0052d9
the1bit <tibor.kiss@cloudmentor.hu>
2025-04-09 23:32:59 +0200

    AWS Clean Up

--- aws/labs/basic/backend.tf
+++ aws/labs/basic/backend.tf
@@ -1,15 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket       = "cloudmentor-lab-terraform-state-bucket"
-#     key          = "labs/basic/terraform.tfstate"
-#     region       = "eu-north-1"
-#     encrypt      = true
-#     use_lockfile = true
-#     profile      = "cloudmentor-lab-terraform"
-#   }
-# }
-
-
 terraform {
   backend "s3" {}
 }
