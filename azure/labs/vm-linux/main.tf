66c711a6ceb9553e729e9e122891a7a80e8b9183
Tibor Kiss <tibor.kiss@cloudmentor.hu>
2025-07-09 15:47:36 +0200

    Lab: vm-linux

---
+++ azure/labs/vm-linux/main.tf
@@ -0,0 +1,21 @@
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.student_username}-rg"
+  location = var.location
+  tags     = local.tags
+  timeouts {
+    create = "60m"
+    delete = "60m"
+  }
+}
+
+module "STUDENT" {
+  source              = "../../modules/student"
+  location            = var.location
+  username            = var.student_username
+  password            = var.student_password
+  resource_group_id   = azurerm_resource_group.rg.id
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
