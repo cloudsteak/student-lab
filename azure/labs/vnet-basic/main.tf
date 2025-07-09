65ad4915d46b89ad8376bb949c17bcabbd9ae5d8
Tibor Kiss <tibor.kiss@cloudmentor.hu>
2025-07-09 15:48:07 +0200

    Remove unnecessary lines

--- azure/labs/vnet-basic/main.tf
+++ azure/labs/vnet-basic/main.tf
@@ -18,14 +18,3 @@ module "STUDENT" {
   resource_group_name = azurerm_resource_group.rg.name
   tags                = local.tags
 }
-
-
-# module "WEBAPP" {
-#   source              = "../../modules/webapp"
-#   location            = var.location
-#   webapp_prefix       = var.username
-#   resource_group_name = azurerm_resource_group.rg.name
-#   os_type             = "Linux"
-#   sku                 = "B1"
-#   tags                = local.tags
-# }
