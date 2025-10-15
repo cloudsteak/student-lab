3e5080d7a5627af947dc6af824ac125d636393d9
the1bit <tibor.kiss@cloudmentor.hu>
2025-10-15 23:18:37 +0200

    Mentor Klub 7 weeks: portal and LB

---
+++ azure/labs/mk-7-01-portal/main.tf
@@ -0,0 +1,31 @@
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.student_username}-rg"
+  location = var.location
+  tags     = local.tags
+  timeouts {
+    create = "60m"
+    delete = "60m"
+  }
+}
+
+module "USER" {
+  source              = "../../modules/user"
+  location            = var.location
+  username            = var.student_username
+  password            = var.student_password
+  resource_group_id   = azurerm_resource_group.rg.id
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+
+# module "WEBAPP" {
+#   source              = "../../modules/webapp"
+#   location            = var.location
+#   webapp_prefix       = var.username
+#   resource_group_name = azurerm_resource_group.rg.name
+#   os_type             = "Linux"
+#   sku                 = "B1"
+#   tags                = local.tags
+# }
