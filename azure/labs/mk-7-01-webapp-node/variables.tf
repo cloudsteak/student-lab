6e9dd8aede1c9700e2384c81efb03f6930e4e0ef
the1bit <tibor.kiss@cloudmentor.hu>
2025-11-13 01:26:02 +0100

    New lab: mk-7-01-webapp-node

---
+++ azure/labs/mk-7-01-webapp-node/variables.tf
@@ -0,0 +1,42 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "student_username" {
+  description = "EntraID username"
+  type        = string
+}
+
+variable "student_password" {
+  description = "Password for the EntraID user"
+  type        = string
+  sensitive   = true
+}
+
+variable "tags" {
+  description = "Tags to be assigned to the resources"
+  type        = map(string)
+  default = {
+    environment = "training"
+    owner       = "cloudmentor"
+    student     = "true"
+    lab         = "mk-7-01-webapp-node"
+  }
+
+}
+
+locals {
+  tags = merge(var.tags, { student_name = var.student_username })
+}
