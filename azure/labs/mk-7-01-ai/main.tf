a2df56175098a1b96d2826de532177ac51a69f3b
the1bit <tibor.kiss@cloudmentor.hu>
2025-11-23 23:33:04 +0100

    mk-7-01-ai

---
+++ azure/labs/mk-7-01-ai/main.tf
@@ -0,0 +1,37 @@
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.student_username}"
+  location = var.location
+  tags     = local.tags
+  timeouts {
+    create = "60m"
+    delete = "60m"
+  }
+}
+
+module "USER" {
+  source              = "../../modules/user"
+  location            = var.location
+  username            = var.student_username
+  password            = var.student_password
+  resource_group_id   = azurerm_resource_group.rg.id
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+module "STORAGE" {
+  source              = "../../modules/storage_account"
+  subscription_id     = var.subscription_id
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+module "AI" {
+  source              = "../../modules/ai"
+  subscription_id     = var.subscription_id
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  rag_storage_account_name = module.STORAGE.storage_account_name
+  tags                = local.tags
+}
